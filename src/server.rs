c8e590fafe52d238e1f3b950c810ddc6add16618
Tuan Anh <anhng.dev@gmail.com>
2024-10-08 23:04:50 +0700

    Add server streaming

--- src/server.rs
+++ src/server.rs
@@ -1,13 +1,45 @@
-use tonic::{transport::Server, Request, Response, Status};
-
 use proto::greeter_server::{Greeter, GreeterServer};
 use proto::{HelloReply, HelloRequest};
+use std::error::Error;
+use std::io::ErrorKind;
+use std::pin::Pin;
+use tokio::sync::mpsc;
+use tonic::codegen::tokio_stream::wrappers::ReceiverStream;
+use tonic::codegen::tokio_stream::{Stream, StreamExt};
+use tonic::{transport::Server, Request, Response, Status, Streaming};
+
+type HelloResult<T> = Result<Response<T>, Status>;
+type ResponseStream = Pin<Box<dyn Stream<Item = Result<HelloReply, Status>> + Send>>;
 
 pub mod proto {
     tonic::include_proto!("helloworld");
 
     pub(crate) const FILE_DESCRIPTOR_SET: &[u8] =
         tonic::include_file_descriptor_set!("helloworld_descriptor");
+}
+
+
+fn match_for_io_error(err_status: &Status) -> Option<&std::io::Error> {
+    let mut err: &(dyn Error + 'static) = err_status;
+
+    loop {
+        if let Some(io_err) = err.downcast_ref::<std::io::Error>() {
+            return Some(io_err);
+        }
+
+        // h2::Error do not expose std::io::Error with `source()`
+        // https://github.com/hyperium/h2/pull/462
+        if let Some(h2_err) = err.downcast_ref::<h2::Error>() {
+            if let Some(io_err) = h2_err.get_io() {
+                return Some(io_err);
+            }
+        }
+
+        err = match err.source() {
+            Some(err) => err,
+            None => return None,
+        };
+    }
 }
 
 #[derive(Debug, Default)]
@@ -26,6 +58,54 @@ impl Greeter for MyGreeter {
         };
 
         Ok(Response::new(reply))
+    }
+
+    type BidirectionalStreamingHelloStream = ResponseStream;
+
+    async fn bidirectional_streaming_hello(
+        &self,
+        request: Request<Streaming<HelloRequest>>,
+    ) -> HelloResult<Self::BidirectionalStreamingHelloStream> {
+        let mut in_stream = request.into_inner();
+        let (tx, rx) = mpsc::channel(128);
+
+        // this spawn here is required if you want to handle connection error.
+        // If we just map `in_stream` and write it back as `out_stream` the `out_stream`
+        // will be dropped when connection error occurs and error will never be propagated
+        // to mapped version of `in_stream`.
+        tokio::spawn(async move {
+            while let Some(result) = in_stream.next().await {
+                match result {
+                    Ok(v) => tx
+                        .send(Ok(HelloReply { message: v.name }))
+                        .await
+                        .expect("working rx"),
+                    Err(err) => {
+                        if let Some(io_err) = match_for_io_error(&err) {
+                            if io_err.kind() == ErrorKind::BrokenPipe {
+                                // here you can handle special case when client
+                                // disconnected in unexpected way
+                                eprintln!("\tclient disconnected: broken pipe");
+                                break;
+                            }
+                        }
+
+                        match tx.send(Err(err)).await {
+                            Ok(_) => (),
+                            Err(_err) => break, // response was dropped
+                        }
+                    }
+                }
+            }
+            println!("\tstream ended");
+        });
+
+        // echo just write the same data that was received
+        let out_stream = ReceiverStream::new(rx);
+
+        Ok(Response::new(
+            Box::pin(out_stream) as ResponseStream
+        ))
     }
 }
 
